fe440fca605821797b58a361a7645574adccec73
jflory7 <jflory7@gmail.com>
2013-07-08 22:35:52 -0400

    Added /tpgo, /definespawn, and /spawn
    See BukkitDev page for more details

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 All You Need
 =============
 
-We are the 2013 Duke TiP students at the University of Georgia taking the Team Programming for Video Games class during the first term of the program. Most of the students in the class began programming for the first time at the beginning of the term in June, and the program lasted three weeks. This specific Bukkit project is a group effort of some of the people from this class, including dobby1498, NightFog125, Hofstadler, Blastprocessor, and jflory7.
+We are the 2013 Duke TiP students at the University of Georgia taking the Team Programming for Video Games class during the first term of the program. Most of the students in the class began programming for the first time at the beginning of the term in June, and the program lasted three weeks. This specific Bukkit project is a group effort of some of the people from this class, including dobby1498, NightFog125, Hofstadler and jflory7.
--- src/com/crystalcraftmc/allyouneed/Main.java
+++ src/com/crystalcraftmc/allyouneed/Main.java
@@ -8,19 +8,37 @@ public final class Main extends JavaPlugin
     public void onEnable()
 	{
         // TODO Insert logic to be performed when the plugin is enabled
-		getLogger().info("onEnable has been invoked!");
+		getLogger().info("All You Need has been enabled!");
+		
+		// Generate the config.yml file.
+		this.saveDefaultConfig();
+		
+		// Load the configuration file and copy the defaults into the plugin...
+		this.getConfig().options().copyDefaults(true);
+		
+		// ...and save the configuration file.
+        this.saveConfig();
 		
 		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
 		getCommand("ct").setExecutor(new AYNCt(this));
 		
 		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
 		getCommand("ender").setExecutor(new AYNEnder(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("tpgo").setExecutor(new AYNTpgo(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("definespawn").setExecutor(new AYNDefineSpawn(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("spawn").setExecutor(new AYNSpawn(this));
     }
  
     @Override
     public void onDisable()
     {
         // TODO Insert logic to be performed when the plugin is disabled
-    	getLogger().info("onDisable has been invoked!");
+    	getLogger().info("All You Need has been disabled!");
     }
 }
