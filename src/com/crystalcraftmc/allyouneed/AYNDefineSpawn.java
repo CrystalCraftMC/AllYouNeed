4a1415e2b66606958d7577e9f034c1f4f3523b94
Beben107700 <ben.delcoigne@gmail.com>
2013-07-13 14:16:00 +0200

    Update AYNDefineSpawn.java
    removed small bug

--- src/com/crystalcraftmc/allyouneed/AYNDefineSpawn.java
+++ src/com/crystalcraftmc/allyouneed/AYNDefineSpawn.java
@@ -17,43 +17,42 @@ public class AYNDefineSpawn implements CommandExecutor
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		// Make the letter 'p' a variable for the command sender (or the player).
-		Player p = (Player) sender;
+		
 		
 	    {	
 	    	// If the player typed /definespawn, then do the following...
 	    	if (cmd.getName().equalsIgnoreCase("definespawn"))
 	    	{
-	    		// If the sender of the command is NOT a player...
-	    		if (!(sender instanceof Player))
-	    		{
-	    			// ...do not let the command be run.
-	    			sender.sendMessage("This command can only be run by a player.");
-	    		}
-	    		// Otherwise...
-	    		else
+	    		if(sender instanceof Player)
 	    		{
 	    			// ...record the world the player is in...
-	    			plugin.getConfig().set("spawn.world", p.getLocation().getWorld().getName());
+	    			plugin.getConfig().set("spawn.world", sender.getLocation().getWorld().getName());
 	    			
 	    			// ...record the x-position the player is in...
-                    plugin.getConfig().set("spawn.x", p.getLocation().getX());
+                    plugin.getConfig().set("spawn.x", sender.getLocation().getX());
                     
                     // ...record the y-position the player is in...
-                    plugin.getConfig().set("spawn.y", p.getLocation().getY());
+                    plugin.getConfig().set("spawn.y", sender.getLocation().getY());
                     
                     // ...record the z-position the player is in...
-                    plugin.getConfig().set("spawn.z", p.getLocation().getZ());
+                    plugin.getConfig().set("spawn.z", sender.getLocation().getZ());
                     
                     // ...save ALL of the above info to the config file...
                     plugin.saveConfig();
                     
                     // ...and tell the player that spawn was set successfully!
-                    p.sendMessage(ChatColor.GREEN + "Spawn set!");
+                    sender.sendMessage(ChatColor.GREEN + "Spawn set!");
 	    		}
 	    		// If this has happened, the function will return true. 
 	    		return true;
 	    	}
+	    	else
+	    		{
+	    			
+	    			sender.sendMessage("This command can only be run by a player.");
+	    			return true;
+	    		}
+	    		
 	    	
 	        // If this hasn't happened, a value of false will be returned.
 	    	return false;
