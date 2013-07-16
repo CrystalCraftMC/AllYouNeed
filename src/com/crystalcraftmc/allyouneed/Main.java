79f76db37ec0d55b7abb169ab87de4b09347c6bb
jflory7 <jflory7@gmail.com>
2013-07-16 01:51:49 -0400

    Fixed auto-update and metrics

--- src/com/crystalcraftmc/allyouneed/Main.java
+++ src/com/crystalcraftmc/allyouneed/Main.java
@@ -1,24 +1,43 @@
 package com.crystalcraftmc.allyouneed;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin
 {
+	// When the plugin first starts...
 	@Override
     public void onEnable()
 	{
         // TODO Insert logic to be performed when the plugin is enabled
 		getLogger().info("All You Need has been enabled!");
 		
-		// Generate the config.yml file.
+		// ...link plugin with online stats.
+		try {
+		    Metrics metrics = new Metrics(this);
+		    metrics.start();
+		} catch (IOException e){
+		    // Failed to submit the stats :-(
+		}
+		
+		// ...generate the config.yml file.
 		this.saveDefaultConfig();
 		
-		// Load the configuration file and copy the defaults into the plugin...
+		// ...load the configuration file and copy the defaults into the plugin...
 		this.getConfig().options().copyDefaults(true);
 		
 		// ...and save the configuration file.
         this.saveConfig();
-		
+        
+        // ...see if the config file allows auto-updating...
+        if (this.getConfig().getBoolean("auto-update"))
+        {
+        	// ...and if so, run the auto-update class.
+        	@SuppressWarnings("unused")
+			Updater updater = new Updater(this, "all-you-need", this.getFile(), Updater.UpdateType.DEFAULT, true);
+        }
+        
 		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
 		getCommand("ct").setExecutor(new AYNCt(this));
 		
@@ -33,6 +52,12 @@ public final class Main extends JavaPlugin
 		
 		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
 		getCommand("spawn").setExecutor(new AYNSpawn(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("definehome").setExecutor(new AYNDefineHome(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("gohome").setExecutor(new AYNGoHome(this));
     }
  
     @Override
